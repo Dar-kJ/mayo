f562d3049594721e01496637da05e703f9cc3094
Shinoa <61569361+shinoa-hiragi@users.noreply.github.com>
2020-10-28 05:33:13 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const { token } = require('./config.json');
 const client = new Discord.Client();
 
 client.once('ready', () => {
-    console.log('Ready.....');
+    console.log('Ready....');
     client.user.setActivity(',,help');
 });
 
