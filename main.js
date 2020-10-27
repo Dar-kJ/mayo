1831af698ba32b893a0b3b7779586327889eb9f3
Shinoa <61569361+shinoa-hiragi@users.noreply.github.com>
2020-10-28 06:12:40 +0800

    Update main.js

--- main.js
+++ main.js
@@ -7,22 +7,22 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-    if(message.content.startsWith === `Who's the founder of this server?`)
+    if(message.content === `Who's the founder of this server?`)
         message.channel.send(`Frankie, our previous owner. They created this server and brought all of these amazing people together in July of 2020.`);
 
-    if(message.content.startsWith === `Where can I apply for staff?`)
+    if(message.content === `Where can I apply for staff?`)
         message.channel.send(`Located in extra lies the staff application channel. You can apply but please do not make any troll applications. They will not be taken seriously and they will be deleted.`);
 
-    if(message.content.startsWith === `Who can I DM for partnerships?`)
+    if(message.content === `Who can I DM for partnerships?`)
         message.channel.send(`Any of our PMS or Seo.`);
 
-    if(message.content.startsWith === `What is Shinyu?`)
+    if(message.content === `What is Shinyu?`)
         message.channel.send(`Close friends of Seo.`);
 
-    if(message.content.startsWith === `Who do I DM if somebody is acting up? `)
+    if(message.content === `Who do I DM if somebody is acting up? `)
         message.channel.send(`Any of the owners or admins. They will respond to the issue as fast as they possibly can.`);
     
-    if(message.content.startsWith === `What are roles?`)
+    if(message.content === `What are roles?`)
         message.channel.send(`They show others who you are and basically your bio. You can locate roles under the member category. If a role is not working, contact a staff member.`);
 });
 
