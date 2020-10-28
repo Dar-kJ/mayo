979b64774102a1511737476d817bceefdee85d99
Shinoa <61569361+shinoa-hiragi@users.noreply.github.com>
2020-10-28 23:21:16 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const preix = ',,';
 
 client.once('ready', () => {
     console.log('Ready....');
@@ -54,6 +55,10 @@ client.on('message', message => {
             if(num === 10)
                 message.channel.send(`${message.author} There are far, far better things ahead than anything we leave behind.`);
         };
+    };
+    if(message.content === `${preix}hotline`) {
+        embed.description = '`Argentina: + 54 (0) 223 493 0430` \n `Amenia: (2) 538194` \n `Australia: 08 93 81 5555` \n `Barbados: (246) 4299999` \n `USA: 911` \n `Zimbawe: (9) 650 00` \n `United Kingdom: +44 (0) 8457 90 90 90` \n `Ukraine: 058` \n `Turkey: 182` \n `Trinidad & Tobago: (868) 645 2800` \n `Tonga: 23000` \n `Hong Kong: +852 28 960 000` `Hungary: (46) 323 888` \n `India: 600 028` \n `Ireland: +44 (0) 8457 90 90 90` \n `Israel: 972-9-8891333` \n `Italy: 800 86 00 22` \n `Japan: +81 (0) 3 5286 9090` \n `Liberia: 6534308` \n `Lithuania: 8-800 2 8888` \n `Malaysia: (06) 284 2500` \n `Malta: 179` \n `Mauritius: 46 48 889` \n `Netherlands: 0602 222 88` \n `New Zealand: (04) 586 1048` \n `Norway: +47 815 33 300` \n `Philippines: 0917 854 9191` \n `Poland: 52 70 000` \n `Portugal: (+351) 225 50 60 70` \n `Russia: 007 (8202) 577-577` \n `Samoa: (+381) 21-6623-393` \n `Serbia: (+381) 21-6623-393` \n `Singapore: 1800- 221 4444` \n `South Africa: 051 444 5691` \n `South Korea: (2) 715 8600` \n `Sri Lanka: 011 057 2222662` \n `St Vincent: (784) 456 1044` \n `Sudan: (249) 11-555-253` \n `Sweden: (46) 31 711 2400` \n `Switzerland: 143` \n `Thailand: (02) 713-6793` \n `Tobago: (868) 645 2800`';
+        message.channel.send({embed: embed});
     };
 });
 
